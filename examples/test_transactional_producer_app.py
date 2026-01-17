ba48145e67be102d1d366c8f8b149c32957a5def
ΜΙΧΑΗΛΙΔΗΣ ΜΙΧΑΗΛ <MICHAILIDIS.MICHAIL@nbg.gr>
2026-01-17 17:20:32 +0200

    transactional producer example updated

--- examples/test_transactional_producer_app.py
+++ examples/test_transactional_producer_app.py
@@ -12,35 +12,46 @@ client = BrokerClient(conf=broker_conf, controller_node=["127.0.0.1", 9877])
 
 queue_1 = "test-1"
 queue_2 = "test-2"
-queue_3 = "test-3"
 
-client.create_queue(queue=queue_1, partitions=3, replication_factor=1)
-client.create_queue(queue=queue_2, partitions=3, replication_factor=1)
-client.create_queue(queue=queue_3, partitions=3, replication_factor=1)
+client.create_queue(queue=queue_1, partitions=1, replication_factor=1)
+client.create_queue(queue=queue_2, partitions=1, replication_factor=1)
 
 transactional_producer = TransactionalProducer(
     client=client,
     producers_info=[
         (ProducerConf(queue=queue_1, wait_ms=5000, max_batch_size=(16384 * 4)), on_delivery_callback),
-        (ProducerConf(queue=queue_2, wait_ms=5000, max_batch_size=(16384 * 4)), on_delivery_callback),
-        (ProducerConf(queue=queue_3, wait_ms=5000, max_batch_size=(16384 * 4)), on_delivery_callback)
+        (ProducerConf(queue=queue_2, wait_ms=5000, max_batch_size=(16384 * 4)), on_delivery_callback)
     ]
 )
 
+tx_id: int = 0
+
 while True:
     try:
-        message = input("Enter a message (type 'exit' to stop): ")
+        do_you_want_to_exit = input("Do you want to exit ? (y/n): ")
 
-        if not message:
+        if do_you_want_to_exit == "y": break
+
+        message = input("Enter a message: ")
+
+        while not message or message == "":
             print("Cannot produce empty message")
-            continue
-
-        if message == "exit": break
+            message = input("Enter a message: ")
 
         key = input("Enter a key for your message (Optional): ")
 
-        transactional_producer.produce(message=message, key=key)
+        tx_id = transactional_producer.begin_transaction()
+
+        transactional_producer.produce(queue_name=queue_1, message=f"| {queue_1} | {message} |", key=key, transaction_id=tx_id)
+        transactional_producer.produce(queue_name=queue_2, message=f"| {queue_2} | {message} |", key=key, transaction_id=tx_id)
+
+        transactional_producer.commit_transaction(tx_id)
+
+        tx_id = 0
     except Exception as e:
         print(f"Could not produce message '{message}'. Reason: {e}")
+
+        if tx_id > 0:
+            transactional_producer.abort_transaction(tx_id)
 
 transactional_producer.close()
